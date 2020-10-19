ca3347008c2925353e3d3b489298172720c49839
OlenkaG <72553571+OlenkaG@users.noreply.github.com>
2020-10-19 17:31:58 +0300

    Update random list.py

--- random list.py
+++ random list.py
@@ -3,10 +3,11 @@ import random
 a=int(input("Enter number FROM: " ))
 b=int(input("Enter number TO: " ))
 
-mylist = random.sample(range(a, b+1),8)
-print("The list is:"+str(mylist))
+
 
 def lastfirst(mylist):
+    mylist = random.sample(range(a, b+1),8)
+    print("The list is:"+str(mylist))
 
     res = [ mylist[0], mylist[-1] ]
     print("\033[33m""The first and last element of list are : ",res)
